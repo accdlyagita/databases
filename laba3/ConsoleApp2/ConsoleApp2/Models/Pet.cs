1b7ee9af5bf27feb4676f303bde40e077ab74319
accdlyagita <115636996+accdlyagita@users.noreply.github.com>
2024-11-26 01:18:29 +0200

    Delete laba3 directory

--- laba3/ConsoleApp2/ConsoleApp2/Models/Pet.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Pet
-{
-    public int PetID { get; set; }
-    public string Name { get; set; }
-    public string Species { get; set; }
-    public string Breed { get; set; }
-    public int Age { get; set; }
-}
